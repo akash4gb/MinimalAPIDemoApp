780f17c54975dd6e01149af64200098585210e9c
Akash Khan <akash4gb@gmail.com>
2022-04-25 23:20:27 +0600

    Added SqlDataAccess class like Generic pattern for Load and save data

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.DbAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data;
+public class UserData
+{
+   private readonly ISqlDataAccess _db;
+   public UserData(ISqlDataAccess db)
+   {
+      _db = db;
+   }
+
+}
