a9183e4f07912d120f3f58a7f151abd6cff693c1
Akash Khan <akash4gb@gmail.com>
2022-04-25 23:40:00 +0600

    Added User data class

--- DataAccess/Data/UserData.cs
+++ DataAccess/Data/UserData.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.DbAccess;
+using DataAccess.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Data;
-public class UserData
+public class UserData : IUserData
 {
    private readonly ISqlDataAccess _db;
    public UserData(ISqlDataAccess db)
@@ -14,4 +15,20 @@ public class UserData
       _db = db;
    }
 
+   public Task<IEnumerable<UserModel>> GetUsers() =>
+      _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+   public async Task<UserModel?> GetUser(int id)
+   {
+      var result = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new { Id = id });
+      return result.FirstOrDefault();
+   }
+
+   public Task InsertUser(UserModel user) =>
+      _db.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
+
+   public Task UpdateUser(UserModel user) =>
+        _db.SaveData("dbo.spUser_Update", user);
+   public Task DeleteUser(int id) =>
+      _db.SaveData("dbo.spUser_Delete", new { Id = id });
 }
